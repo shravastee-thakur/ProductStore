48a601aa903365069f521d126a4c5b219459d7f5
Shravastee Thakur <shravastee@gmail.com>
2024-11-01 14:42:03 +0530

    backend completed

---
+++ backend/routes/product.route.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+import {
+  createProducts,
+  deleteProduct,
+  getProducts,
+  updateProduct,
+} from "../controllers/product.controller.js";
+
+const router = express.Router();
+
+router.get("/", getProducts);
+
+router.post("/", createProducts);
+
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
+
+export default router;
